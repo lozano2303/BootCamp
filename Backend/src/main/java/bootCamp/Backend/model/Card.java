16f284d5ddb4444ce743dbb3324d785a94dffe09
cristofer lozano <cristoferlozano233@gmail.com>
2025-08-01 06:25:56 -0500

    Mer, incio backend

---
+++ Backend/src/main/java/bootCamp/Backend/model/Card.java
@@ -0,0 +1,5 @@
+package bootCamp.Backend.model;
+
+public class Card {
+
+}
---
+++ Backend/src/main/java/bootCamp/Backend/model/Player.java
@@ -0,0 +1,8 @@
+package bootCamp.Backend.model;
+
+import jakarta.persistence.Entity;
+
+@Entity (name = "player")
+public class Player {
+
+}
